1a7591db614f4705506bd11f461501ba499edbfe
omid-mousavi <omidmousavi266@gmail.com>
2024-03-19 11:43:51 +0330

    added hover logic

--- javascript.js
+++ javascript.js
@@ -1,28 +1,48 @@
 function setGridSize() {
   let sizePixel = parseInt(prompt('Whole integer\nMax: 100', '16'));
   if(isNaN(sizePixel)) {
-    return;
+    
   } else if(sizePixel <= 100 && sizePixel > 0) {
     resetAll();
     for(let row = 1; row <= sizePixel; row++) {
-      pixelContainerCreate = document.createElement('div');
-      pixelContainerCreate.classList.add('pixel-container');
-      container.appendChild(pixelContainerCreate);
+      pixelContainer = document.createElement('div');
+      pixelContainer.classList.add('pixel-container');
+      container.appendChild(pixelContainer);
       for(let column = 1; column <= sizePixel; column++) {
         pixel = document.createElement('div');
         pixel.classList.add('pixel');
-        pixelContainerCreate.appendChild(pixel);
+        pixelContainer.appendChild(pixel);
       }
     }
   } else {
     alert('Try again...');
   }
+  pixel = document.querySelectorAll('.pixel');
+  pixel.forEach((element) => {
+    element.addEventListener('mousedown', colorizePixelManage)
+  })
 }
 function setGridColor() {
 
 }
+function colorizePixelManage(event) {
+  event.target.style.backgroundColor = 'blue'; // for test
+  pixel.forEach((element) => {
+    element.addEventListener('mouseover', colorizePixel);
+    element.addEventListener('mousedown', removeMouseOverEffet)
+  })
+}
+function colorizePixel(event) {
+  event.target.style.backgroundColor = 'blue'; // for test
+}
+function removeMouseOverEffet() {
+  pixel.forEach((element) => {
+    element.removeEventListener('mouseover', colorizePixel);
+    element.removeEventListener('mousedown', removeMouseOverEffet)
+  })
+}
 function resetAll() {
-  let pixelContainer = document.querySelectorAll('.pixel-container')
+  pixelContainer = document.querySelectorAll('.pixel-container')
   if(container.hasChildNodes()) {
     pixelContainer.forEach((ele) => {
       ele.remove();
@@ -33,5 +53,7 @@ function resetAll() {
 const container = document.querySelector('.container');
 const setGridSizeBtn = document.querySelector('.set-grid-size');
 
-let pixelContainerCreate, pixel;
+let pixelContainer, pixel;
+
+
 setGridSizeBtn.addEventListener('click', setGridSize);
